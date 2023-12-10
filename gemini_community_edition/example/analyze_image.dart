5b3dc6a7c153574467ab20b1453c36c4092798b6
Frank van Puffelen <puf@google.com>
2023-12-09 16:11:59 -0800

    Make analyze_image example more versatile

--- gemini_community_edition/example/analyze_image.dart
+++ gemini_community_edition/example/analyze_image.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:gemini_community_edition/src/common/content.dart';
+import 'package:gemini_community_edition/src/common/generative_content_response.dart';
 import 'package:gemini_community_edition/src/common/model_name.dart';
 import 'package:gemini_community_edition/src/common/part.dart';
 import 'package:gemini_community_edition/src/generative_model.dart';
@@ -8,22 +9,26 @@ import 'dart:io';
 
 final apiKey = File("example/API_KEY").readAsStringSync();
 
-void main() async {
-  print("Let's try some basic image recognition. What's in this picture, Gemini?");
-  proVisionModelStreaming();
+void main(List<String> arguments) async {
+  proVisionModel(arguments.isNotEmpty ? arguments : ["What is in this picture?", "example/duck.png"]);
 }
 
-Future proVisionModel() async {
-  final file = File('example/scones.jpg');
-  final bytes = file.readAsBytesSync();
-  final base64 = base64Encode(bytes);
-  print('Loaded ${bytes.length} bytes and converted into a base64 string of ${base64.length} chars');
+Future proVisionModel(List<String> args) async {
+  final parts = parseArgumentsIntoParts(args);
   final model = GenerativeModel(ModelName.geminiProVision, apiKey);
-  final response = await model.generateContent(Content("user", [
-    TextPart("What is in this picture?"),
-    Base64ImagePart("image/jpeg", base64)
-  ]));
-  print(response);
+  final response = await model.generateContent(Content("user", parts));
+  printResponse(response);
+}
+
+void proVisionModelStreaming(List<String> args) {
+  final parts = parseArgumentsIntoParts(args);
+  final model = GenerativeModel(ModelName.geminiProVision, apiKey);
+  model.generateContentStream(Content("user", parts)).listen((response) {
+    printResponse(response);
+  });
+}
+
+void printResponse(GenerateContentReponse response) {
   final candidates = response.candidates;
   if (candidates != null) {
     for (var candidate in candidates) {
@@ -40,30 +45,24 @@ Future proVisionModel() async {
   }
 }
 
-void proVisionModelStreaming() {
-  final file = File('example/scones.jpg');
-  final bytes = file.readAsBytesSync();
-  final base64 = base64Encode(bytes);
-  print('Loaded ${bytes.length} bytes and converted into a base64 string of ${base64.length} chars');
-  final model = GenerativeModel(ModelName.geminiProVision, apiKey);
-  model.generateContentStream(Content("user", [
-    TextPart("What is in this picture?"),
-    Base64ImagePart("image/jpeg", base64)
-  ])).listen((response) {
-    print(response);
-    final candidates = response.candidates;
-    if (candidates != null) {
-      for (var candidate in candidates) {
-        print(candidate.content.parts.length);
-        for (var part in candidate.content.parts) {
-          if (part is TextPart) {
-            print(part.text);
-          }
-        }
-      }
+List<Part> parseArgumentsIntoParts(List<String> args) {
+  final parts = args.map((arg) {
+    final file = File(arg);
+    if (File(arg).existsSync()) {
+      final base64String = base64Encode(file.readAsBytesSync());
+      print(arg.substring(arg.lastIndexOf(".")));
+      final mimeType = switch (arg.substring(arg.lastIndexOf("."))) {
+        ".png" => "image/png",
+        ".jpg" => "image/jpeg",
+        ".jpeg" => "image/jpeg",        
+        String() => throw "Unhandled file type: $arg",
+      };
+      print('Loaded $arg as a $mimeType part');
+      return Base64ImagePart(mimeType, base64String);
     }
     else {
-      print("Got no answers");
+      return TextPart(arg);
     }
   });
+  return parts.toList();
 }
